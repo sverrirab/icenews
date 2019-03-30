653f38d97b174bbf66b3d892d0b2bff8dddd1fbe
Sverrir Á. Berg <sab@keilir.com>
2019-03-30 15:34:25 +0700

    Adding tox and travis configurations

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
-setup(name='icenews',
-      version='1.0',
-      description='Simple NLP of Icelandic News',
-      url='http://github.com/sverrirab/icenews',
-      author='Sverrir A. Berg',
-      author_email='sab@keilir.com',
-      license='Apache',
-      packages=['icenews'],
-      zip_safe=False)
+setup(
+    name='icenews',
+    version='1.0',
+    description='Simple NLP for Icelandic News',
+    url='http://github.com/sverrirab/icenews',
+    author='Sverrir A. Berg',
+    author_email='sab@keilir.com',
+    license='Apache',
+    packages=[
+        'icenews'
+    ],
+    zip_safe=False,
+    install_requires=[
+        'Flask-RESTful>=0.3.7',
+        'reynir>=1.5.1',
+    ]
+)
